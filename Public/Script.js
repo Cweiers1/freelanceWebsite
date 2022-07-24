77e03df608bc425069b8e6ce7de5c046a3f90289
Cweiers1 <Cweiers1@gmail.com>
2022-07-23 20:16:29 -0400

    is still working on dark mode

--- Public/Script.js
+++ Public/Script.js
@@ -1,3 +1,8 @@
+let Site = document.getElementById("html");
+//loads previous theme if one was saved
+let previousTheme = (localStorage.getItem("Theme"));
+let currentTheme = "light";
+
 //find OS theme via the prefers-color-theme CSS media qurey
 function findOSTheme() {
     if (window.matchMedia("(prefers-color-theme: dark)")) {
@@ -7,15 +12,30 @@ function findOSTheme() {
     }
 }
 
-//find previous saved theme
-let previousTheme = (localStorage.getItem("Theme"));
-let currentTheme = "light";
-
+function loadTheme() {
 //if no previous saved theme, set theme based on OS preference
-if (previousTheme != "light" || "dark") {
-   currentTheme = findOSTheme() 
-} else {
-    currentTheme = previousTheme;
+    if (previousTheme != "light" || "dark") {
+        currentTheme = findOSTheme();
+    } else {
+        currentTheme = previousTheme;
+    }
 }
+     
+    
 
+//sets theme passed to function, and if save = true, overrides localStorage saved theme
+function setTheme(Theme, save) {
+    switch(Theme) {
+        case "None":
+            localStorage.removeItem("Theme");
+        break;
+        case "light":
+        case "dark":
+            Site.classList.add(Theme);
+        break;
+        }
+    if (save == true && Theme != "None") {
+        localStorage.setItem("Theme", Theme);
+    }
+}
 
