b90d4ffb37a1787af0aabbb39dad8151a0ade0e0
Cweiers1 <Cweiers1@gmail.com>
2022-07-25 23:40:38 -0400

    Revamped darkmode saving

--- Public/Script.js
+++ Public/Script.js
@@ -2,14 +2,17 @@ let Site = document.getElementById("html");
 
 // loads previous theme if one was saved
 let previousTheme = (localStorage.getItem("Theme"));
+let OSprefer = window.matchMedia("(prefers-color-scheme: dark)");
 
-let OSTheme = "";
+OSprefer.addEventListener( "change", );
 
 //uses CSS media queries to find OS theme
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    OSTheme = "dark";
-} else {
-    OSTheme = "light";
+function OSTheme() {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark";
+    } else {
+        return "light";
+    }
 }
 
 function loadTheme(loadData) {
@@ -21,26 +24,41 @@ function loadTheme(loadData) {
         case "light":
             Site.dataset.theme = "light";
         break;
+        case "OSTheme":
+            Site.dataset.theme = OSTheme();
+        break;
         default:
-            Site.dataset.theme = OSTheme;
+            Site.dataset.theme = OSTheme();
     }
 }
      
 // sets theme passed to function, and if save = true, overrides localStorage saved theme
 function setTheme(Theme, save) {
-    if (Theme == "dark" || "light") {
+    if (Theme === "dark" || "light") {
         Site.dataset.theme = Theme;
-        if (save == true) {
-            localStorage.setItem("Theme", Theme);
+        if (save) {
+            saveTheme(Theme);
         } 
-    } else {
-        localStorage.removeItem("Theme");
-        Site.dataset.theme = OSTheme;
     } 
 }
+
+//clears theme and set it to OS default
+function clearTheme() {
+    localStorage.removeItem("theme");
+    Site.dataset.theme = OSTheme();
+}
+
+
+// if no theme is stored on startup
+if (localStorage.getItem("Theme") == null) {
+    localStorage.setItem("Theme", "OSTheme");
+}
+
+
+
 
 loadTheme(previousTheme);
 
 
-
+console.log(OSTheme())
 
